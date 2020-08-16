a23c2fbc26ac857eeb240492e2b96cf39576fd28
John Ko <jko121790@gmail.com>
2020-08-16 10:24:21 -0700

    accumlate product complete

--- medium/accumulating-product/code.js
+++ medium/accumulating-product/code.js
@@ -1,5 +1,11 @@
 function accumulatingProduct(arr) {
-  // Your code here.
+  let accumulate = 0;
+  const newArray = [];
+  for (let i = 0; i < arr.length; i++) {
+    accumulate *= arr[i];
+    newArray.push(accumulate);
+  }
+  return newArray;
 }
 
 module.exports = accumulatingProduct;
