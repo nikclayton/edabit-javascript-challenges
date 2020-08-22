f62ba67dbf64f11b6e51c00bc4b74c56ee1be90b
John Ko <jko121790@gmail.com>
2020-08-22 10:37:34 -0700

    are letters in second string present in first complete

--- medium/are-letters-in-the-second-string-present-in-the-first/code.js
+++ medium/are-letters-in-the-second-string-present-in-the-first/code.js
@@ -1,5 +1,12 @@
 function letterCheck(arr) {
-  // Your code here.
+  const firstWord = arr[0].toLowerCase();
+  const secondWord = arr[1].toLowerCase();
+  for (let i = 0; i < secondWord.length; i++) {
+    if (!firstWord.includes(secondWord[i])) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = letterCheck;
