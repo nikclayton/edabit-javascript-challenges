e46ecc5d4b96944a97cbeb26a9d13bcf5cdb85f4
John Ko <jko121790@gmail.com>
2020-08-25 09:16:57 -0700

    barbecue challenge complete

--- medium/barbecue-skewers/code.js
+++ medium/barbecue-skewers/code.js
@@ -1,5 +1,17 @@
 function bbqSkewers(grill) {
-  // Your code here.
+  let veggie = 0;
+  let nonVeggie = 0;
+  const portions = [];
+  for (let i = 0; i < grill.length; i ++) {
+    if (!grill[i].includes("x")) {
+      veggie++;
+    } else {
+      nonVeggie++;
+    }
+  }
+  portions.push(veggie);
+  portions.push(nonVeggie);
+  return portions;
 }
 
 module.exports = bbqSkewers;
