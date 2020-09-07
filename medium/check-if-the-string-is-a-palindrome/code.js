a6068a60fe67ac795df1d7189fdf2205370b39ed
John Ko <jko121790@gmail.com>
2020-09-07 10:34:37 -0700

    check palindrome complete

--- medium/check-if-the-string-is-a-palindrome/code.js
+++ medium/check-if-the-string-is-a-palindrome/code.js
@@ -1,6 +1,12 @@
 function isPalindrome(str) {
-  let newStr = str.replace(" ", "");
-  console.log(newStr, str)
+  let newStr = str.replace(/ /g, "").replace(/,/g, "").replace(/[^\w]/g, "").toLowerCase();
+  const strLength = newStr.length - 1;
+  for (let i = 0; i < newStr.length; i++) {
+    if(newStr[i] !== newStr[strLength - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = isPalindrome;
