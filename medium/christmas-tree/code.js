4912be6322b4861971aa7b3d411340c392e163af
John Ko <jko121790@gmail.com>
2020-09-09 13:51:10 -0700

    attempted tree

--- medium/christmas-tree/code.js
+++ medium/christmas-tree/code.js
@@ -1,5 +1,28 @@
 function tree(h) {
-  // Your code here.
+  const cTree = [];
+  if(h === 1) {
+    cTree.push("#")
+    return cTree;
+  }
+  for (let i = 1; i < h + 1; i++) {
+    let layer = "";
+    for (let k = 1; k < h - i ; k++) {
+      layer += " ";
+    }
+    for (let j = 0; j < h; j++) {
+      if (j === 0) {
+        layer += "#";
+      } else  {
+        layer += "##"
+      }
+    }
+    for (let k = 1; k < h - i; k++) {
+      layer += " ";
+    }
+    console.log(cTree)
+    cTree.push(layer);
+  }
+  return cTree;
 }
 
 module.exports = tree;
