4fd10b0b75d48f9661129314aa35a3c19826345a
John Ko <jko121790@gmail.com>
2020-09-21 00:59:52 -0700

    delete extra elements

--- medium/delete-occurrences-of-extra-elements-in-an-array/code.js
+++ medium/delete-occurrences-of-extra-elements-in-an-array/code.js
@@ -1,5 +1,18 @@
 function deleteOccurrences(arr, num) {
-  // Your code here.
+  const obj = {};
+  const newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if(obj.hasOwnProperty(arr[i])) {
+      obj[arr[i]]++;
+
+    } else {
+      obj[arr[i]] = 1;
+    }
+    if (obj[arr[i]] <= num) {
+      newArr.push(arr[i])
+    }
+  }
+  return newArr;
 }
 
 module.exports = deleteOccurrences;
