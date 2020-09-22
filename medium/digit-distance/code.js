f8b9a9ffb05d78f82e6c140b539b5847a213daf9
John Ko <jko121790@gmail.com>
2020-09-22 08:33:06 -0700

    digit distance complete

--- medium/digit-distance/code.js
+++ medium/digit-distance/code.js
@@ -1,5 +1,15 @@
 function digitDistance(num1, num2) {
-  // Your code here.
+  const str1 = num1 + "";
+  const str2 = num2 + "";
+  let distance = 0;
+  for (let i = 0; i < str1.length; i++) {
+    let difference = parseInt(str1[i]) - parseInt(str2[i]);
+    if (difference < 0) {
+      difference *= -1;
+    }
+    distance += difference;
+  }
+  return distance;
 }
 
 module.exports = digitDistance;
