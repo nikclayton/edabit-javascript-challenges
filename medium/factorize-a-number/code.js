423ed37fda08dbd2114dbdff48b8362efaf32154
John Ko <jko121790@gmail.com>
2020-09-26 10:38:44 -0700

    factorize complete

--- medium/factorize-a-number/code.js
+++ medium/factorize-a-number/code.js
@@ -1,5 +1,11 @@
 function factorize(num) {
-  // Your code here.
+  const factors = [];
+  for (let i = 1; i <= num; i++) {
+    if (num % i === 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
 }
 
 module.exports = factorize;
