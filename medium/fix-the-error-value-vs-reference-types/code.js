f0930e6ebe37fc9a2ef04f29e9c79f57c3889e30
John Ko <jko121790@gmail.com>
2020-09-27 01:49:46 -0700

    fixed error value vs reference

--- medium/fix-the-error-value-vs-reference-types/code.js
+++ medium/fix-the-error-value-vs-reference-types/code.js
@@ -1,5 +1,10 @@
 function checkEquals(arr1, arr2) {
-  // Your code here.
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = checkEquals;
