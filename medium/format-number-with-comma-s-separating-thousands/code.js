ca4732150a6ea1ecc1a413633e8c642658d3c7be
John Ko <jko121790@gmail.com>
2020-09-29 09:13:08 -0700

    format attempted

--- medium/format-number-with-comma-s-separating-thousands/code.js
+++ medium/format-number-with-comma-s-separating-thousands/code.js
@@ -1,5 +1,11 @@
 function formatNum(num) {
-  // Your code here.
+  let number = (num + "").split("");
+  console.log(number)
+  for (let i = number.length - 1; i > 0; i - 3) {
+    number.splice(i, ",");
+  }
+  const result = number.join("")
+  return result;
 }
 
 module.exports = formatNum;
