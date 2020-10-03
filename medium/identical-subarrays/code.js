79797c10a8454b9c753d22756fbad3ec31f83bb8
John Ko <jko121790@gmail.com>
2020-10-03 00:16:53 -0700

    identical attempt

--- medium/identical-subarrays/code.js
+++ medium/identical-subarrays/code.js
@@ -1,5 +1,21 @@
 function countIdentical(arr) {
-  // Your code here.
+  let counter = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length === 1) {
+      counter++;
+    } else {
+      let equal = true;
+      for (let k = 1; k < arr[i].length; k++) {
+        if (arr[i][0] !== arr[i][k]) {
+          equal = false;
+        }
+      }
+      if (equal) {
+        counter++;
+      }
+    }
+    return  counter;
+  }
 }
 
 module.exports = countIdentical;
