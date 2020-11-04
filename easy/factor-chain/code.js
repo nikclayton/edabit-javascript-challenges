2dddfa559ef72404a632f8ebb6e5026d7f8a7d17
John Ko <jko121790@gmail.com>
2020-11-04 07:16:52 -0800

    factor chain complete

--- easy/factor-chain/code.js
+++ easy/factor-chain/code.js
@@ -1,5 +1,10 @@
 function factorChain(arr) {
-  // Your code here.
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] % arr[i] !== 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = factorChain;
