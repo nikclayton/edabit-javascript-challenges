ce36426a37342d1faa9ae4e5f315a49521d42484
John Ko <jko121790@gmail.com>
2020-11-08 00:10:52 -0800

    find min and max complete

--- easy/find-the-smallest-and-biggest-numbers/code.js
+++ easy/find-the-smallest-and-biggest-numbers/code.js
@@ -1,5 +1,19 @@
 function minMax(arr) {
-  // Your code here.
+  const newArr = [];
+  let min = arr[0];
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i]
+    }
+  }
+  newArr.push(min);
+  newArr.push(max);
+
+  return newArr;
 }
 
 module.exports = minMax;
