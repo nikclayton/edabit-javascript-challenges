b43b1e5ed761e788c1b245524c9bd0310b7762b8
John Ko <jko121790@gmail.com>
2020-11-13 00:05:46 -0800

    joini two portion complete

--- easy/join-two-portions-of-a-path/code.js
+++ easy/join-two-portions-of-a-path/code.js
@@ -1,5 +1,11 @@
 function joinPath(portion1, portion2) {
-  // Your code here.
+  if (portion1[portion1.length - 1] === "/" && portion2[0] === "/") {
+    return portion1.substr(0,  portion1.length - 1) + portion2;
+  } else if (portion1[portion1.length - 1] === "/" || portion2[0] === "/") {
+    return portion1 + portion2;
+  } else {
+    return portion1 + "/" + portion2;
+  }
 }
 
 module.exports = joinPath;
