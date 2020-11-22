62f3bde660dd2aa60e5abb0538c9d633b450a385
John Ko <jko121790@gmail.com>
2020-11-22 00:22:23 -0800

    snail

--- easy/snail-race/code.js
+++ easy/snail-race/code.js
@@ -1,5 +1,15 @@
 function mauriceWins(mSnails, sSnails) {
-  // Your code here.
+  let wins = 0;
+
+  if (mSnails[0] > sSnails[2]) {
+    wins++;
+  }
+  if (mSnails[1] > sSnails[0]) {
+    wins++
+  }
+  if (mSnails[2] > sSnails[1]) {
+    wins++
+  }
 }
 
 module.exports = mauriceWins;
