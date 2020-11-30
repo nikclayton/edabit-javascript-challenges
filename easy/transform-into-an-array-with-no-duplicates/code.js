fa95c52bf25388134f9a5375234184010d8e128a
John Ko <jko121790@gmail.com>
2020-11-30 07:18:25 -0800

    transform no dup

--- easy/transform-into-an-array-with-no-duplicates/code.js
+++ easy/transform-into-an-array-with-no-duplicates/code.js
@@ -1,5 +1,10 @@
 function set(arr) {
-  // Your code here.
+  const obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (!obj.hasOwnProperty(arr[i])) {
+      obj[arr[i]] = arr[i]
+    }
+  }
 }
 
 module.exports = set;
