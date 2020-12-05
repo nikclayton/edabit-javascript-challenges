ff0164e4f4819168d4de713fefad9e03d6f0c2af
John Ko <jko121790@gmail.com>
2020-12-05 03:14:04 -0800

    upvote

--- easy/transform-upvotes/code.js
+++ easy/transform-upvotes/code.js
@@ -3,7 +3,7 @@ function transformUpvotes(str) {
   const strArr = str.split(" ");
   for(let i = 0; i < strArr.length; i++) {
     if (strArr[i].indexOf("k") !== -1) {
-
+      let newNum = strArr[i].replace(/k/g, "");
     }
   }
 }
