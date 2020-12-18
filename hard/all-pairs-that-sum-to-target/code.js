332bf42fc277f579cf32d2e5b8e3172991114e55
John Ko <jko121790@gmail.com>
2020-12-17 23:57:16 -0800

    pairs sums

--- hard/all-pairs-that-sum-to-target/code.js
+++ hard/all-pairs-that-sum-to-target/code.js
@@ -3,7 +3,7 @@ function allPairs(arr, target) {
   for (let i = 0; i < arr.length - 1; i++) {
     for (let k = i + 1; k < arr.length ; k++) {
       if (arr[i] + arr[k] === target) {
-
+        const tempArr = [];
       }
     }
   }
