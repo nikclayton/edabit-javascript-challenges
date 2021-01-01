0bc448b879b9b04a90749d20b5a9772ae1e8256b
John Ko <jko121790@gmail.com>
2021-01-01 01:08:17 -0800

    beginning and end pairs

--- hard/beginning-and-end-pairs/code.js
+++ hard/beginning-and-end-pairs/code.js
@@ -1,5 +1,11 @@
 function pairs(arr) {
   const newArr = [];
+  let arrHalf = null;
+  if (arr.length % 2 === 0) {
+    arrHalf = arr.length / 2;
+  } else {
+    arrHalf = (arr.length - 1) / 2;
+  }
 }
 
 module.exports = pairs;
