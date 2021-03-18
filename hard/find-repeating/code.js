9856dd6d7bc84b2c1fa3b263bc190be20d9e586c
John Ko <jko121790@gmail.com>
2021-03-18 00:06:26 -0700

    finding repeat

--- hard/find-repeating/code.js
+++ hard/find-repeating/code.js
@@ -1,5 +1,13 @@
 function findRepeating(str) {
-  // Your code here.
+  const arr = [];
+  let stack = 0;
+  const tempArr = [];
+  for (let i = 0; i < str.length; i++)  {
+    let current = str[i];
+    if (stack === 0) {
+      tempArr.push(str[i]);
+    }
+  }
 }
 
 module.exports = findRepeating;
