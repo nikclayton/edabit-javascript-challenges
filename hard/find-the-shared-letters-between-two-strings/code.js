c3ec00665b7d4eb5a6be80543b6dd8fd264571da
John Ko <jko121790@gmail.com>
2021-03-21 08:50:57 -0700

    shared letter

--- hard/find-the-shared-letters-between-two-strings/code.js
+++ hard/find-the-shared-letters-between-two-strings/code.js
@@ -1,12 +1,16 @@
 function sharedLetters(str1, str2) {
   const copies  =  [];
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
   for (let i = 0; i < str1.length; i++) {
-    if (str2.includes(str1[i]))  {
+    if (str2.includes(str1[i]) && !copies.includes(str1[i]))  {
       copies.push(str1[i]);
     }
   }
 
-  return copies;
+  copies.sort();
+
+  return copies.join("");
 }
 
 module.exports = sharedLetters;
