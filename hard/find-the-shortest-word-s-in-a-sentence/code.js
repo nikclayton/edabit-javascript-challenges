363b73afef94990b39f68a579e1ac8874a2b473d
John Ko <jko121790@gmail.com>
2021-03-22 00:35:13 -0700

    find the shortest

--- hard/find-the-shortest-word-s-in-a-sentence/code.js
+++ hard/find-the-shortest-word-s-in-a-sentence/code.js
@@ -1,5 +1,21 @@
 function findShortestWords(str) {
-  // Your code here.
+  const words = str.split(" ");
+  const wordLength = [];
+  const shortest = [];
+  let min = words[0].length;
+  for (let i = 0; i < words.length; i++) {
+    wordLength.push(words[i].length);
+    if (min > words[i].length) min = words[i].length;
+  }
+
+  for (let i = 0; i < words.length; i++)  {
+    if (words[i].length === min) {
+      shortest.push(words[i].toLowerCase());
+    }
+  }
+
+  console.log(words, wordLength, min);
+  return shortest.sort();
 }
 
 module.exports = findShortestWords;
