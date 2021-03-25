52f06da22e11ce2bf8d3069927dc7df94f918ef9
John Ko <jko121790@gmail.com>
2021-03-25 00:31:14 -0700

    flip array

--- hard/flip-the-array/code.js
+++ hard/flip-the-array/code.js
@@ -1,5 +1,14 @@
 function flipArray(arr) {
+  const newArr = [];
   let columns = arr[0].length;
+
+
+  if (columns === undefined) {
+    for (let i = 0; i < arr.length; i++) {
+      newArr.push([arr[i]]);
+    }
+  }
+
 
   console.log(arr[0], columns)
 }
