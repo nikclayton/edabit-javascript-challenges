38ab2b2c601361de406591be07fc772674cbd4fa
John Ko <jko121790@gmail.com>
2021-03-29 23:58:53 -0700

    fridge poetry

--- hard/fridge-poetry/code.js
+++ hard/fridge-poetry/code.js
@@ -1,5 +1,15 @@
 function canBuild(s1, s2) {
-  // Your code here.
+  const string1 = s1.replace(/ /g, '');
+  const string2 = s2.replace(/ /g, '');
+
+  for (let i = 0; i < s1.length; i++) {
+    if (!s1.includes(s2[i]))  {
+      return false;
+    }
+  }
+
+  console.log(string1, string2);
+  return true;
 }
 
 module.exports = canBuild;
