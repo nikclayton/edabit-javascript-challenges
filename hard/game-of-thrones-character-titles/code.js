d5672ae5dbaa7662180c177f10efff60af0991b1
John Ko <jko121790@gmail.com>
2021-04-01 00:03:59 -0700

    game of throne name

--- hard/game-of-thrones-character-titles/code.js
+++ hard/game-of-thrones-character-titles/code.js
@@ -1,5 +1,15 @@
 function correctTitle(str) {
-  // Your code here.
+ const words = str.split(" ")
+ for (let i = 0; i < words.length; i++) {
+   const currentWord = words[i].toLowerCase();
+  if (currentWord  === "and" || currentWord === "the" || currentWord === "of"
+      || currentWord === "in") {
+        words[i] = currentWord;
+      } else {
+        words[i]  = currentWord[0].toUpperCase() + currentWord.substring(1, currentWord.length);
+      }
+ }
+ return words.join(" ")
 }
 
 module.exports = correctTitle;
