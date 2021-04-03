7e6d0f366041484ea8ef32e68b11bf44f4dfde9d
John Ko <jko121790@gmail.com>
2021-04-03 01:14:02 -0700

    gold distribution

--- hard/gold-distribution/code.js
+++ hard/gold-distribution/code.js
@@ -1,5 +1,18 @@
 function piratesKilled(gold, tolerance) {
-  // Your code here.
+  let max = 0;
+  for (let i = 0; i < gold.length; i++) {
+    if (max < gold[i])  {
+      max = gold[i]
+    }
+  }
+  for (let i = 0; i < gold.length; i++) {
+    console.log(tolerance[i], (max - gold[i]))
+    if (tolerance[i] <= (max - gold[i])) {
+      return true;
+    }
+  }
+  console.log(max);
+  return false;
 }
 
 module.exports = piratesKilled;
