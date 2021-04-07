049b3447bc49a4e9ed545177ece11d5f04f0759d
John Ko <jko121790@gmail.com>
2021-04-07 06:32:41 -0700

    potions

--- hard/growing-and-shrinking-potions/code.js
+++ hard/growing-and-shrinking-potions/code.js
@@ -1,5 +1,21 @@
 function afterPotion(str) {
-  // Your code here.
+  const arr = str.split("");
+  let isGrow = false;
+  let isShrink = false;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === "A") isGrow = true;
+    if (arr[i] === "B") isShrink = true;
+    if (isGrow) {
+      arr[i]  = (parseInt(arr[i]) + 1) + "";
+      isGrow = false;
+    }
+    if (isShrink) {
+      arr[i] = (parseInt(arr[i]) - 1) + "";
+      isShrink = false;
+    }
+  }
+
+  return  str.join("");
 }
 
 module.exports = afterPotion;
