1d9125d3036af02c0347a1c0e6b0fe8417c7aa03
John Ko <jko121790@gmail.com>
2021-04-08 23:54:53 -0700

    hashtag

--- hard/headline-hash-tags/code.js
+++ hard/headline-hash-tags/code.js
@@ -1,5 +1,28 @@
 function getHashTags(str) {
-  // Your code here.
+  const longestWords = [];
+  const words = str.split(" ")
+  let longest = null;
+  let secondLongest = null;
+  let thirdLongest = null;
+
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > longest) {
+      thirdLongest = secondLongest;
+      secondLongest = longest;
+      longest = words[i];
+    }  else if (words[i].length > secondLongest) {
+      thirdLongest = secondLongest;
+      secondLongest = words[i];
+    } else if (words[i].length > thirdLongest)  {
+      thirdLongest = words[i]
+    }
+  }
+
+  longestWords.push(`# ${longest}`);
+  longestWords.push(`# ${secondLongest}`);
+  longestWords.push(`# ${thirdLongest}`);
+
+  return longestWords
 }
 
 module.exports = getHashTags;
