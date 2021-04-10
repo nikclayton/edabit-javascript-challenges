e1103bf308159266ad25087125554be8fb96f799
John Ko <jko121790@gmail.com>
2021-04-10 00:01:58 -0700

    hashtag

--- hard/headline-hash-tags/code.js
+++ hard/headline-hash-tags/code.js
@@ -1,16 +1,16 @@
 function getHashTags(str) {
   const longestWords = [];
   const words = str.split(" ")
-  let longest = null;
-  let secondLongest = null;
-  let thirdLongest = null;
+  let longest = "";
+  let secondLongest = "";
+  let thirdLongest = "";
 
   for (let i = 0; i < words.length; i++) {
-    if (words[i].length > longest) {
+    if (words[i].length > longest.length) {
       thirdLongest = secondLongest;
       secondLongest = longest;
       longest = words[i];
-    }  else if (words[i].length > secondLongest) {
+    }  else if (words[i].length > secondLongest.length) {
       thirdLongest = secondLongest;
       secondLongest = words[i];
     } else if (words[i].length > thirdLongest)  {
