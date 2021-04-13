8120e0ea2bca7b8d52741199697d1b4e0c5f5b86
John Ko <jko121790@gmail.com>
2021-04-12 23:59:17 -0700

    rows and columns

--- hard/identical-row-and-column/code.js
+++ hard/identical-row-and-column/code.js
@@ -1,5 +1,14 @@
 function hasIdentical(arr) {
-  // Your code here.
+  const columns = [];
+  for (let i = 0; i < arr.length; i ++) {
+    const column = [];
+    for (let k = 0; k <arr[i].length; k++) {
+      column.push(arr[k][i])
+    }
+    columns.push(column)
+  }
+
+  console.log(columns)
 }
 
 module.exports = hasIdentical;
