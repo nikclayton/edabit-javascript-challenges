404341d30269d83d9718f874595d658e0d1577f2
John Ko <jko121790@gmail.com>
2021-04-21 00:04:48 -0700

    is a string

--- hard/is-a-string-stretched/code.js
+++ hard/is-a-string-stretched/code.js
@@ -11,11 +11,8 @@ function isStretched(s1, s2) {
   }
 
   for (let i = 0; i < s1.length; i += inRow) {
-    console.log(i);
     newStr += s1[i]
   }
-
-  console.log(newStr, inRow);
 
   if (newStr ===  s2)  return true;
   return false;
