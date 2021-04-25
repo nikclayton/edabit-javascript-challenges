b3be3959f809c8d317bf2b749de3fd0d31cd1d56
John Ko <jko121790@gmail.com>
2021-04-24 23:59:15 -0700

    larger than right

--- hard/larger-than-all-numbers-to-their-right/code.js
+++ hard/larger-than-all-numbers-to-their-right/code.js
@@ -11,12 +11,13 @@ function largerThanRight(arr) {
 
   console.log(largestNumber, isLargest)
 
-  if (isLargest) {
-    largerNumber.push(largestNumber);
+  if (isLargest === true) {
+    largerNumber.push(arr[0]);
+    console.log(largerNumber)
   }
 
   for (let i = 1; i < arr.length; i++) {
-    if (largestNumber < arr[i] && !largerNumber.includes(arr[i])) {
+    if (arr[0] < arr[i] && !largerNumber.includes(arr[i])) {
       largerNumber.push(arr[i])
     }
   }
